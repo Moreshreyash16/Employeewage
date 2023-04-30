02502a846ce5fbe2a30988a7e963f676dc87d475
Moreshreyash16 <mshreyash108@gmail.com>
2023-04-30 20:35:38 +0530

    uc5

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,31 +2,38 @@
 package Bridgelabz.Employeewg;
 
 public class EmployeeWage {
+    public static final int PART_TIME = 1;
+    public static final int FULL_TIME = 2;
+    public static final int Emp_RATE_PER_HR = 20;
+    public static final int NUM_OF_WORKING_DAYS=20;
+
     public static void main(String[] args) {
-        final int PART_TIME=1;
-        final int FULL_TIME=2;
-        final int Emp_RATE_PER_HR=20;
+        int EmpHr = 0;
+        int EmpWage = 0;
+        int totalEmpWage = 0;
 
-        int EmpHr=0;
-        int EmpWage=0;
-        int empcheck= (int) (Math.floor(Math.random()* 10 )% 3);
+        for (int day=0; day< NUM_OF_WORKING_DAYS;day++) {
+            int empcheck = (int) (Math.floor(Math.random() * 10) % 3);
 
-        switch(empcheck)
-        {
-            case PART_TIME:
-                EmpHr=4;
-                break;
+            switch (empcheck) {
+                case PART_TIME:
+                    EmpHr = 4;
+                    break;
 
-            case FULL_TIME:
-                EmpHr=8;
-                break;
+                case FULL_TIME:
+                    EmpHr = 8;
+                    break;
 
-            default:
-                EmpHr=0;
+                default:
+                    EmpHr = 0;
+            }
+            EmpWage = EmpHr * Emp_RATE_PER_HR;
+            totalEmpWage += EmpWage;
+            System.out.println("Employee Wage : " + EmpWage);
         }
-        EmpWage=EmpHr*Emp_RATE_PER_HR;
-        System.out.println("Employee Wage : " + EmpWage);
+        System.out.println("Toatal Emp Wage : " + totalEmpWage);
 }
+
 }
 
 
